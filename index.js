2066222d28868b7e5ebd9a14e9971dd68e6549bd
Chris Gibbons <christ487@icloud.com>
2023-09-26 20:17:58 +0100

    correct .value

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ let qrtext = document.getElementById("qrtext");
 function generateQR() {
   qrimage.src =
     "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-    qrtext.ariaValueMax;
+    qrtext.value;
 }
