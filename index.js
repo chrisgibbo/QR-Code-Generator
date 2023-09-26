c6790c8ecc3622df36e9de650f421d75ac9f4fa8
Chris Gibbons <christ487@icloud.com>
2023-09-26 20:38:06 +0100

    add else statement to js with error css for 1s

--- index.js
+++ index.js
@@ -8,5 +8,10 @@ function generateQR() {
       "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
       qrtext.value;
     imageBox.classList.add("show-img");
+  } else {
+    qrtext.classList.add("error");
+    setTimeout(() => {
+      qrtext.classList.remove("error");
+    }, 1000);
   }
 }
