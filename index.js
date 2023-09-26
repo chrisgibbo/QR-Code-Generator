4dc3d9d3a53401f0cdfef0fbfb3f0b34025b837d
Chris Gibbons <christ487@icloud.com>
2023-09-26 20:29:17 +0100

    add an if statement to function

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ let qrimage = document.getElementById("qrimage");
 let qrtext = document.getElementById("qrtext");
 
 function generateQR() {
-  qrimage.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-    qrtext.value;
-  imageBox.classList.add("show-img");
+  if (qrtext.value.length > 0) {
+    qrimage.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrtext.value;
+    imageBox.classList.add("show-img");
+  }
 }
