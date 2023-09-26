da9547c1aea9f79dfc9b4a7d6b18aaf99b175b33
Chris Gibbons <christ487@icloud.com>
2023-09-26 21:48:23 +0100

    adjust class in const, add copyButton func

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-let imageBox = document.getElementById("imageBox");
-let qrimage = document.getElementById("qrimage");
-let qrtext = document.getElementById("qrtext");
+const imageContainer = document.getElementById("imageContainer");
+const qrimage = document.getElementById("qr-image");
+const qrtext = document.getElementById("qr-text");
 
 function generateQR() {
   if (qrtext.value.length > 0) {
     qrimage.src =
       "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
       qrtext.value;
-    imageBox.classList.add("show-img");
+    imageContainer.classList.add("show-img");
   } else {
     qrtext.classList.add("error");
     setTimeout(() => {
       qrtext.classList.remove("error");
     }, 1000);
+  }
+}
+
+function copyButton() {
+  const range = document.createRange();
+  range.selectNode(imageContainer);
+
+  const selection = window.getSelection();
+
+  selection.removeAllRanges();
+  selection.addRange(range);
+  try {
+    document.execCommand("copy");
+  } catch (err) {
+    console.error("Unable to copy image to clipboard", err);
+  } finally {
+    selection.removeAllRanges();
   }
 }
