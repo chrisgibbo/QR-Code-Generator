d85ab53d17f10768f111d8114cb83ef53117d9c4
Chris Gibbons <christ487@icloud.com>
2023-09-26 20:13:07 +0100

    import api from qr gen into func

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ let imageBox = document.getElementById("imageBox");
 let qrimage = document.getElementById("qrimage");
 let qrtext = document.getElementById("qrtext");
 
-function generateQR() {}
+function generateQR() {
+  qrimage.src =
+    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+    qrtext.ariaValueMax;
+}
